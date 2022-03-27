e5d9f8b172c8ad0b62377718093e0a073b27754b
FernandoA5 <lvl5.games.solutions@gmail.com>
2022-03-27 14:55:16 -0600

    Terminado código de fuerzas, ya se muestra en pantalla

--- app/src/main/java/com/example/aceleracin/MainActivity.kt
+++ app/src/main/java/com/example/aceleracin/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.aceleracin
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+import android.os.Bundle
+import com.example.aceleracin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var sensorManager: SensorManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //SI AGREGAMOS UN BOTON
+        //binding.calculateButton.setOnClickListener { boton() }
+        setUpSensorStuff()
+    }
+    private fun setUpSensorStuff()
+    {
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also {
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_FASTEST, SensorManager.SENSOR_DELAY_FASTEST)
+        }
+    }
+    override fun onSensorChanged(event: SensorEvent?) {
+        val stringX = binding.textoX.text.toString()
+        if(event?.sensor?.type == Sensor.TYPE_ACCELEROMETER){
+            binding.textoX.text= getString(R.string.fuerza_en_x, event.values[0].toString())
+            binding.textoY.text= getString(R.string.fuerza_en_y, event.values[1].toString())
+            binding.textoZ.text= getString(R.string.fuerza_en_z, event.values[2].toString())
+        }
+    }
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        return
+    }
+    override fun onDestroy() {
+        sensorManager.unregisterListener(this)
+        super.onDestroy()
     }
 }
+
